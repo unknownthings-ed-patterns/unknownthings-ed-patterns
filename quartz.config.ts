3284be13eb7a10c9fb447a041818b90cfb75f90a
Your Name <your@email.com>
2026-05-02 08:25:08 +0900

    initial commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -88,8 +88,8 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+     
+     
     ],
   },
 }

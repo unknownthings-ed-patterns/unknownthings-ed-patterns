b5295da000a8ef90f31450e87095ae06fa7c49e2
Your Name <your@email.com>
2026-05-02 08:45:19 +0900

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "unknownthings-ed-patterns.github.io/unknownthings-ed-patterns",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
